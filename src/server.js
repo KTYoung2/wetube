11832e2196566bbf79bd3a30bfeb28b6b64c81f9
kty <xodud9625@hanmail.net>
2022-12-22 18:04:33 +0900

    commit

--- src/server.js
+++ src/server.js
@@ -1,24 +1,13 @@
 //server 설정하기 express application
 
 import express from "express";
-//import -> node_module에서 express 불러옴.
+//import -> node_module에서express 불러와 함수설정
+import morgan from "morgan";
+// morgan -> node.js용 request logger middleware 
 
 //express function을 사용하면 알아서 application생성됨
 const app = express();
-
-//middleware도 req,res, next가 필요함 -> next를 호출한다면 이 함수는 미들웨어임
-const middleWare = (req, res, next) => {
-   console.log(`${req.method} ${req.url}`);
-   next();
-};
-
-const privateMiddleware = (req, res, next) => {
-   const url = req.url;
-   if(url === "/protected") {
-      return res.send("<h1> Not Allowed </h1>")
-   }
-   next();
-};
+const logger = morgan("dev"); 
 
          //1. reques object, 2. response object
 const handleHome = (req, res) => {
@@ -26,21 +15,14 @@ const handleHome = (req, res) => {
    //응답함수
 };
 
-const handleProtected = (res, req) => {
-   return res.send("Welcome to private");
-};
 
 
-//전역 미들웨어를 만들 수 있게해줌(어느 url에도 작동하는)
-app.use(middleWare);
-app.use(privateMiddleware);
-
+app.use(logger);
 // application(sever)설정 -> request 요청하기 
 app.get("/", handleHome);
-app.get("/protected", handleProtected);
 
 
-
+               
 const handleListening = () => console.log("Server Listening on port 4000~~");
 
 /* server는 사용자가 요청할때까지 기다리게 해야함. 
