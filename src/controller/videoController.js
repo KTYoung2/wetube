5b808f75f3dcd61d074bd574be71641201cb3137
kty <xodud9625@hanmail.net>
2022-12-28 17:58:26 +0900

    commit

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -1,12 +1,41 @@
 //controller는 함수, router는 그 함수를 이용하는 입장이기 때문에 분리하는 것 
-export const tranding = (req, res) => 
-    res.render("home", {pageTitle : "Home"});
+export const tranding = (req, res) => {
+   const videos = [
+     {
+        title : "First Videos",
+        rating : 5,
+        comment : 2,
+        createdAt : "2 miuntes ago",
+        views : 60,
+        id : 1
+     },
+     {
+        title : "Second Videos",
+        rating : 5,
+        comment : 2,
+        createdAt : "2 miuntes ago",
+        views : 60,
+        id : 1
+     },
+     {
+        title : "Third Videos",
+        rating : 5,
+        comment : 2,
+        createdAt : "2 miuntes ago",
+        views : 60,
+        id : 1
+     },
+    ];
+    
+   return res.render("home", {pageTitle : "Home" ,  videos});
     //pug 렌더링 render("view name");
+};
+
 export const see = (req, res) => 
-    res.render("watch", {pageTitle : "Watch"});
+    res.render("watch");
 
 export const edit = (req, res) => 
-     res.render("edit", {pageTitle : "Edit"});
+     res.render("edit");
 
 export const search = (req, res) => res.send("search");
 export const upload = (req, res) => res.send("Upload");
