6a160055bcfdc1abb0246a06043c1c9de94c9c4c
kty <xodud9625@hanmail.net>
2022-12-30 11:06:07 +0900

    commit

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -4,7 +4,7 @@ let videos = [
        rating : 5,
        comment : 2,
        createdAt : "2 miuntes ago",
-       views : 60,
+       views : 1,
        id : 1,
     },
     {
@@ -28,16 +28,28 @@ let videos = [
 
 //controller는 함수, router는 그 함수를 이용하는 입장이기 때문에 분리하는 것 
 export const tranding = (req, res) => {
-  
    return res.render("home", {pageTitle : "Home" ,  videos});
     //pug 렌더링 render("view name");
 };
 
-export const see = (req, res) => 
-    res.render("watch");
+export const watch = (req, res) => {
+   const { id } = req.params;
+   const video = videos[id - 1];
+   return res.render("watch", {pageTitle : `Watching ${video.title}`, video});
+};
 
-export const edit = (req, res) => 
-     res.render("edit");
+export const getEdit = (req, res) => {
+   const { id } = req.params;
+   const video = videos[id - 1];
+   return res.render("edit", {pageTitle :`Editing: ${video.title}`, video});
+}; 
+
+export const postEdit = (req, res) => {
+   const { id } = req.params;
+   const { title } = req.body;
+   videos[id - 1].title = title;
+   return res.redirect(`/videos/${id}`);
+};
 
 export const search = (req, res) => res.send("search");
 export const upload = (req, res) => res.send("Upload");
@@ -45,6 +57,6 @@ export const deleteVideo = (req, res) => {
     console.log(req.params);
     return res.send("DeleteVideo");
 
-}
+};
 
 /** export -> 하나 이상의 함수를 공유하고 싶을 떄 */
--- src/router/videoRouter.js
+++ src/router/videoRouter.js
@@ -1,15 +1,14 @@
 import express from "express";
 //js파일들은 하나의 모듈, 독립되어 있음 그러니 한 파일 안에서도 돌아가는 환경을 또 만들어줘야함.
-import { see, edit,deleteVideo, upload } from "../controller/videoController";   
+import { watch, getEdit, postEdit, deleteVideo, upload } from "../controller/videoController";   
         // ../ -> 지금 있는 폴더에서 벗어나는 걸 의미  ./ -> 지금의 장소
 
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", upload);
-videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)", watch);
                     //숫자만 들어갈 수 있게 설정
-videoRouter.get("/:id(\\d+)/edit", edit);
-videoRouter.get("/:id(\\d+)/delete", deleteVideo);
+videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.get("/upload", upload);
 
 export default videoRouter;
 /** export default
